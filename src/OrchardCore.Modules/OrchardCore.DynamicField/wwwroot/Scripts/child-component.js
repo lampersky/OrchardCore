1049a53eefab83f16e706693fd1e51121b6ded02
lampersky <lampersky@gmail.com>
2025-08-06 09:53:13 +0200

    example

--- src/OrchardCore.Modules/OrchardCore.DynamicField/wwwroot/Scripts/child-component.js
+++ src/OrchardCore.Modules/OrchardCore.DynamicField/wwwroot/Scripts/child-component.js
@@ -9,20 +9,27 @@ class ChildComponent extends HTMLElement {
           <div>
             <h4>Child Component</h4>
             <p>Parent value is: <span id="parentVal">Unknown</span></p>
-            <input></input>
+            <div>
+            <input name="latitude"></input>
+            <input name="longitude"></input>
+            </div>
           </div>`;
 
-        const input = this.shadowRoot.querySelector('input');
-        input.addEventListener('input', () => {
-            this.dispatchEvent(new CustomEvent('value-changed', {
-                detail: {
-                    now: new Date().toJSON(),
-                    input: input.value
-                },
-                bubbles: true,
-                composed: true,
+        this.latitude = this.shadowRoot.querySelector('input[name="latitude"]');
+        this.longitude = this.shadowRoot.querySelector('input[name="longitude"]');
+
+        [this.latitude, this.longitude].forEach(input =>
+            input.addEventListener('input', () => {
+                this.dispatchEvent(new CustomEvent('value-changed', {
+                    detail: {
+                        now: new Date().toJSON(),
+                        latitude: this.latitude.value,
+                        longitude: this.longitude.value,
+                    },
+                    bubbles: true,
+                    composed: true,
+                }));
             }));
-        });
 
         this.addEventListener('value-changed', (event) => {
             this.updateValue(event.detail);
@@ -30,7 +37,8 @@ class ChildComponent extends HTMLElement {
     }
 
     updateValue(detail) {
-        this.shadowRoot.querySelector('input').value = detail.input;
+        this.latitude.value = detail.latitude;
+        this.longitude.value = detail.longitude;
         this.shadowRoot.querySelector('#parentVal').textContent = JSON.stringify(detail);
     }
 }
