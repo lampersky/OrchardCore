b4f9d9887b0b00233b602884764bcf8076f2b709
lampersky <lampersky@gmail.com>
2025-08-18 23:59:33 +0200

    wip

--- src/OrchardCore.Modules/OrchardCore.DynamicField/Settings/DynamicFieldSettingsDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.DynamicField/Settings/DynamicFieldSettingsDisplayDriver.cs
@@ -18,13 +18,6 @@ public sealed class DynamicFieldSettingsDisplayDriver : ContentPartFieldDefiniti
             model.ScriptUrls = settings.ScriptUrls;
             model.StyleUrls = settings.StyleUrls;
             model.ScriptModuleUrls = settings.ScriptModuleUrls;
-            if (settings.Resources.Count == 0)
-            {
-                for (var i = 0; i < 5; i++)
-                {
-                    settings.Resources.Add(new());
-                }
-            }
             model.Resources = settings.Resources;
         }).Location("Content");
     }
