90a74498db5f4b7d902925b901cbe8d574c89526
lampersky <lampersky@gmail.com>
2025-08-24 16:34:20 +0200

    wip

--- src/OrchardCore.Modules/OrchardCore.DynamicFields/Extensions/DynamicFieldSettingsExtensions.cs
+++ src/OrchardCore.Modules/OrchardCore.DynamicFields/Extensions/DynamicFieldSettingsExtensions.cs
@@ -5,6 +5,9 @@ using OrchardCore.DynamicFields.Settings;
 namespace OrchardCore.DynamicFields.Extensions;
 public static class DynamicFieldSettingsExtensions
 {
+    const string scriptPrefix = "dynamicFields.";
+    const string stylePrefix = "#";
+
     public static DynamicFieldSettings Fix(this DynamicFieldSettings dynamicFieldSettings,
         string clonedContentType, string clonedContentField, string contentType, string contentField)
     {
@@ -13,19 +16,32 @@ public static class DynamicFieldSettingsExtensions
             return dynamicFieldSettings;
         }
 
-        var cloned = $"dynamicField.{clonedContentType.ToSafeName()}_{clonedContentField.ToSafeName()}_{nameof(DynamicField.Value)}";
-        var newName = $"dynamicField.{contentType.ToSafeName()}_{contentField.ToSafeName()}_{nameof(DynamicField.Value)}";
+        var cloned = $"{clonedContentType.ToSafeName()}_{clonedContentField.ToSafeName()}_{nameof(DynamicField.Value)}";
+        var newName = $"{contentType.ToSafeName()}_{contentField.ToSafeName()}_{nameof(DynamicField.Value)}";
+
+        cloned = $"{scriptPrefix}{cloned}";
+        newName = $"{scriptPrefix}{newName}";
 
         if (!string.IsNullOrEmpty(dynamicFieldSettings.Code))
         {
             dynamicFieldSettings.Code = dynamicFieldSettings.Code.Replace(cloned, newName);
+            //dynamicFieldSettings.Code = dynamicFieldSettings.Code.Replace(cloned, newName);
         }
 
         foreach (var resource in dynamicFieldSettings.Resources)
         {
-            if (resource.IsScript && resource.IsInline && !string.IsNullOrEmpty(resource.Src))
+            if (resource.IsInline && !string.IsNullOrEmpty(resource.Src))
+            {
+                continue;
+            }
+
+            if (resource.IsScript)
             {
                 resource.Src = resource.Src.Replace(cloned, newName);
+            }
+            else if (resource.IsStyle)
+            {
+                //resource.Src = resource.Src.Replace(cloned, newName);
             }
         }
 
